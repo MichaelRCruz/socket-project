3b9b099980a8ab1eaf201fb373989608a577a7f4
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-18 13:32:14 -0700

    adds delayed event

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var sup = [1, 2, 3]
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-//Whenever someone connects this gets executed
 io.on('connection', function(socket){
   console.log('A user connected');
 
-  //Whenever someone disconnects this piece of code executed
+  setTimeout(function() {
+    // socket.send('Sent message 4 seconds after the connection!');
+    socket.send(sup);
+  }, 4000);
+
   socket.on('disconnect', function () {
     console.log('A user disconnected');
   });

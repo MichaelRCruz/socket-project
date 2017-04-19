f0aa78fb07aa415078cce258ec35d6501b26d450
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-19 13:09:16 -0700

    constructs chat room

--- app.js
+++ app.js
@@ -2,40 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var sup = [1, 2, 3]
-
-var clients = 0;
-
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
-
+users = [];
 io.on('connection', function(socket){
   console.log('A user connected');
-  clients++;
-  socket.emit('newclientconnect', { description: 'Hey, welcome!' });
-  socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' });
-
-  socket.on('disconnect', function() {
-    console.log('A user disconnected');
-    socket.on('disconnect', function () {
-	       clients--;
-	         io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
-             });
+  socket.on('setUsername', function(data){
+    console.log(data);
+    if(users.indexOf(data) > -1){
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+    else{
+      users.push(data);
+      socket.emit('userSet', {username: data});
+    }
   });
-
-  socket.on('clientEvent', function(data) {
-    console.log(data)
-  });
-
+  socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+  })
 });
-
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket){
-  console.log('someone connected');
-  nsp.emit('hi', 'Hello everyone!');
-});
-
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on localhost:3000');
 });
